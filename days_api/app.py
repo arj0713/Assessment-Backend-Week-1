44260622a0105fc2c70529615eee903f66b7ba56
arj0713 <arj07@live.co.uk>
2024-02-16 12:17:49 +0000

    Add /between post route

--- days_api/app.py
+++ days_api/app.py
@@ -25,7 +25,22 @@ def add_to_history(current_request):
 @app.get("/")
 def index():
     """Returns an API welcome messsage."""
-    return jsonify({ "message": "Welcome to the Days API." })
+    return jsonify({"message": "Welcome to the Days API."})
+
+
+@app.route("/between", methods=["POST"])
+def post_for_days_between():
+    """Returns the days between two posted dates"""
+    data = request.json
+    if not ("first" in data or "last" in data):
+        return jsonify({"error": "Missing required data."}), 400
+    try:
+        first = convert_to_datetime(data["first"])
+        last = convert_to_datetime(data["last"])
+    except ValueError:
+        return jsonify({"error": "Unable to convert value to datetime."}), 400
+    days = get_days_between(first, last)
+    return jsonify({"days": days}), 200
 
 
 if __name__ == "__main__":
